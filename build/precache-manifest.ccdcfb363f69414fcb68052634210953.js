0db6ebe78c723ea82b6063928e885967b41a7456
Guillaume <guillaume.badi@gmail.com>
2018-12-24 15:23:10 +0100

    build

---
+++ build/precache-manifest.ccdcfb363f69414fcb68052634210953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d1a464eaf212faf1b75a6956ca69d04",
+    "url": "/static/media/noise.4d1a464e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f45b721c68bf368d3b48",
+    "url": "/static/js/main.f45b721c.chunk.js"
+  },
+  {
+    "revision": "c9c2abad73d4968e24d5",
+    "url": "/static/js/1.c9c2abad.chunk.js"
+  },
+  {
+    "revision": "f45b721c68bf368d3b48",
+    "url": "/static/css/main.a0eee065.chunk.css"
+  },
+  {
+    "revision": "5f68814df8013db74bda47b7053a7282",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b58cf19e8611fecf00188da98f0f92b6.js"
+  "/precache-manifest.ccdcfb363f69414fcb68052634210953.js"
 );
 
 workbox.clientsClaim();
